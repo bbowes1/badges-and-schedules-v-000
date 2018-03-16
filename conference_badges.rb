08528bbcbb5ec9a357bac575127add21556d046a
Benjamin Bowes <bbowes999@gmail.com>
2018-03-16 16:31:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+attendees.map do |attendee|
+  badge_maker(attendee)
+   
+ end
+end
+
+def assign_rooms(attendees)
+  attendees.collect.each_with_index do |value, index| 
+   "Hello, #{value}! You'll be assigned to room #{index + 1}!"
+end
+ 
+
+
+end
+
+def printer(attendees)
+  print batch_badge_creator(attendees)
+attendees.each do |attendee|
+  
+
+
+  
+
+ end
+end
